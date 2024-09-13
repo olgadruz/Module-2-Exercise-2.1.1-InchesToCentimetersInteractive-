44c2a6f34756b55ea81cd122d5cd7510cafd70c8
Оля Дружкова <Druzhkova@MacBook-Pro-Ola.local>
2024-09-13 14:42:28 -0400

    first commit

---
+++ InchesToCentimetersIntera.cs
@@ -0,0 +1,13 @@
+﻿namespace InchesToCentimetersInteractive;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double inch = 2.54;
+        string cm = Console.ReadLine();
+        string[] cmww = cm.Split(" ");
+        double result = Convert.ToDouble(cmww[0]);
+        Console.WriteLine(result * inch + " centimeters");
+    }
+}
